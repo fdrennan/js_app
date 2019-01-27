5fb7cc99624cf67f718cf882fddb9fe230245ce4
Freddy Drennan <drennanfreddy@gmail.com>
2019-01-27 16:18:31 -0700

    update

---
+++ main.js
@@ -0,0 +1,40 @@
+b1 = $("#box1");
+b2 = $("#box2");
+var fadeIn;
+
+fadeIn = true;
+
+b1.click(()  => {
+    b1.fadeOut(1000)
+      .fadeIn(750)
+      .fadeOut(500)
+      .fadeIn(250)
+  });
+
+
+
+  var trace1 = {
+    x: [1, 2, 3, 4],
+    y: [10, 15, 13, 17],
+    mode: 'markers',
+    type: 'scatter'
+  };
+  
+  var trace2 = {
+    x: [2, 3, 4, 5],
+    y: [16, 5, 11, 9],
+    mode: 'lines',
+    type: 'scatter'
+  };
+  
+  var trace3 = {
+    x: [1, 2, 3, 4],
+    y: [12, 9, 15, 12],
+    mode: 'lines+markers',
+    type: 'scatter'
+  };
+  
+  var data = [trace1, trace2, trace3];
+  
+  Plotly.newPlot('box4', data);
+
